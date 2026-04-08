eb451f491a008a214beb85e7c032ccd8b74b2349
vyshnavikona09-crypto <vyshnavikona09@gmail.com>
2026-04-08 23:32:50 +0530

    Create tasks.py

---
+++ env/tasks.py
@@ -0,0 +1,19 @@
+TASKS = {
+    "easy": [
+        {"text": "Swiggy order ₹200", "amount": 200, "label": "Food"},
+        {"text": "Uber ride ₹150", "amount": 150, "label": "Transport"},
+        {"text": "Electricity bill ₹1200", "amount": 1200, "label": "Bills"},
+    ],
+    
+    "medium": [
+        {"text": "Amazon purchase ₹800", "amount": 800, "label": "Shopping"},
+        {"text": "Zomato dinner ₹400", "amount": 400, "label": "Food"},
+        {"text": "Petrol ₹1000", "amount": 1000, "label": "Transport"},
+    ],
+    
+    "hard": [
+        {"text": "Flipkart grocery order", "amount": 600, "label": "Food"},
+        {"text": "Recharge ₹299", "amount": 299, "label": "Bills"},
+        {"text": "Online order ₹700", "amount": 700, "label": "Shopping"},
+    ]
+}
